15dfd31f9b96fb0bd496be6c07bab00e78bdc593
Artem <artchzhen@gmail.com>
2023-04-21 17:38:16 +0900

    added new protocol Picture, and write code to view remember - how mach image was show

--- project1/New Project1/ViewController.swift
+++ project1/New Project1/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var pictures = [String]()
+    var pictures = [Picture]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +16,31 @@ class ViewController: UITableViewController {
         title = "Storm Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-        print(items)
-        
-        for item in items {
-            if item.hasPrefix("nssl") {
-                pictures.append(item)
+        let defaults = UserDefaults.standard
+        if let savedPictures = defaults.object(forKey: "savedPictures") as? Data {
+            let jsonDecoder = JSONDecoder()
+                        do {
+                pictures = try jsonDecoder.decode([Picture].self, from: savedPictures)
+            } catch {
+                print("Failed to load data")
             }
         }
-        print(pictures)
         
-        pictures.sort()
+        if pictures.isEmpty {
+            let fm = FileManager.default
+            let path = Bundle.main.resourcePath!
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            //        print(items)
+            
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    let picture = Picture(image: item, views: 0)
+                    pictures.append(picture)
+                }
+            }
+            //        print(pictures)
+            //        pictures.sort()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,19 +48,32 @@ class ViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        let picture = pictures[indexPath.item]
+        cell.textLabel?.text = picture.image
+        cell.textLabel?.text = "Viewed: \(picture.views)"
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let dc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            dc.selectedImage = pictures[indexPath.row]
+            
+            let picture = pictures[indexPath.item]
+            dc.selectedImage = picture.image
             dc.selectedPictureNumber = indexPath.row + 1
             dc.totalPictures = pictures.count
+            picture.views += 1
+            save()
+            tableView.reloadData()
             navigationController?.pushViewController(dc, animated: true)
-            
-            
         }
-       
+    }
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(pictures) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "pictures")
+        } else {
+            print("Failed to save pictures.")
+        }
     }
 }
