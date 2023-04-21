675664a2d86e49b2c1f7589ee0aafa66298d3462
Artem <artchzhen@gmail.com>
2023-04-21 18:44:50 +0900

    added new protocol and use Codable, write code with save results game flags

--- project2/project2/ViewController.swift
+++ project2/project2/ViewController.swift
@@ -15,13 +15,14 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var theBestResult = [TheBestResults]()
     var correctAnswer = 0
     var stepCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score", style: .plain, target: self, action: #selector(scoreSum))
-       
         
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
@@ -33,11 +34,24 @@ class ViewController: UIViewController {
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
+        let defaults = UserDefaults.standard
+        if let saveHighScore = defaults.object(forKey: "saveScore") as? Data {
+            let jsonDecoder = JSONDecoder()
+            do {
+                theBestResult = try jsonDecoder.decode([TheBestResults].self, from: saveHighScore)
+            } catch {
+                print("Failed to load high score")
+            }
+        }
+        
+        if theBestResult.isEmpty {
+            let highScore = TheBestResults(highestScore: 0)
+            theBestResult.append(highScore)
+        }
         
         askQuestion()
     }
     
-
     func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
@@ -45,13 +59,12 @@ class ViewController: UIViewController {
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
-        
-//        title = "\(countries[correctAnswer].uppercased()) you score = \(score)"
+
         title = "\(countries[correctAnswer].uppercased()) you use attempt = \(stepCount)"
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
-        var title: String
+        var title: String?
         stepCount += 1
         
         if sender.tag == correctAnswer {
@@ -62,21 +75,37 @@ class ViewController: UIViewController {
             title = "Wrong!That’s the flag \(countries[sender.tag])"
             score -= 1
         }
-        if stepCount == 10
-        {
-            title = "You have \(score) score, and You have done \(stepCount)"
+        
+        if stepCount == 4 {
+            let highScore = theBestResult[0]
+            if score > highScore.highestScore {
+                highScore.highestScore = score
+                save()
+            }
+            let ac = UIAlertController(title: "Game Over!", message: "This game: \(score) Highest: \(highScore.highestScore)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Quit", style: .default, handler: askQuestion))
+            present(ac, animated: true)
             stepCount = 0
             score = 0
+        } else {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
         }
-        
-        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        present(ac, animated: true)
     }
-    @objc func scoreSum() {
-         let scoreAlert = UIAlertController(title: "Score", message: nil, preferredStyle: .actionSheet)
-         scoreAlert.addAction(UIAlertAction(title: "Your score \(score) points", style: .default, handler: nil))
-         
-         present(scoreAlert, animated: true)
+        @objc func scoreSum() {
+            let scoreAlert = UIAlertController(title: "Score", message: nil, preferredStyle: .actionSheet)
+            scoreAlert.addAction(UIAlertAction(title: "Your score \(score) points", style: .default, handler: nil))
+            present(scoreAlert, animated: true)
+        }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(theBestResult) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "savedScore")
+        } else {
+            print("Failed to save high score.")
+        }
     }
 }
